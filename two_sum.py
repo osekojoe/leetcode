dce001067621f5c64783ce01ff2e799c11601f9c
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-24 12:58:36 +0100

    return indices of two numbers adding up to target

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,12 @@
 #!usr/bin/env python3
 class Solution(object):
+	"""return indices of the two numbers such that they 
+	add up to target
+	eg: 
+	Input: nums = [2,7,11,15], target = 9
+	Output: [0,1]
+	Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+	"""
 	def twoSum(self, nums, target):
 		"""
 		:type nums: List[int]
