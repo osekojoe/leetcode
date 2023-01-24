634b2730ab21d2d8a6857f7fd4433bf8ed3324e6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-24 12:55:48 +0100

    return indices of two numbers adding up to target

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+class Solution(object):
+	def twoSum(self, nums, target):
+		"""
+		:type nums: List[int]
+		:type target: int
+		:rtype: List[int]
+		"""
+		for i in range(len(nums)):
+			for j in range((i + 1), len(nums)):
+				if nums[j] == target - nums[i]:
+					return [i, j]
+
+nums = [2,7,11,15]
+target = 9
+
+sol = Solution.twoSum(object, nums, target)
+print(sol)
